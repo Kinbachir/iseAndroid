d0a63e339271fb3717b1e1cceb9d5ad1dad669b6
saoussen <saoussen.boulakbech@gmail.com>
2019-07-23 19:08:07 +0200

    commit_6 (refresh app)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -425,15 +425,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         textView2.setText("Veuillez vérifier votre connexion internet \n et réessayez");
     }
 
-    public void reload() {
-        Intent intent = getIntent();
-        overridePendingTransition(0, 0);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        onStop();
-        overridePendingTransition(0, 0);
-        startActivity(intent);
-    }
-
     private void PageSiteInactif() {
         linearLayout1.setVisibility(LinearLayout.GONE);
         linearLayout2.setVisibility(LinearLayout.VISIBLE);
